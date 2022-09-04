1208b78bb987aa21e77d22f3ee74dea667957a34
GuilledlC <guillermodelacalhacar@gmail.com>
2022-09-04 20:32:01 +0200

    Reworked everything to support multiple clients based entirely on https://www.youtube.com/c/WittCode 's tutorial

--- src/Client.java
+++ src/Client.java
@@ -1,20 +1,85 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Client extends SendReceive {
-    protected String ip;
-    public Client() throws IOException {
+public class Client{
+
+    protected Socket socket;
+    protected BufferedReader bufferedReader;
+    protected BufferedWriter bufferedWriter;
+    protected String username;
+
+    public Client(Socket socket, String username) {
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            this.username = username;
+        } catch (IOException e) {
+            close(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    public void sendMessages() {
+        try {
+            bufferedWriter.write(username);
+            bufferedWriter.newLine();
+            bufferedWriter.flush();
+
+            Scanner scanner = new Scanner(System.in);
+            while (socket.isConnected()) {
+                String message = scanner.nextLine();
+                bufferedWriter.write(username + ": " + message);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
+            }
+        } catch (IOException e) {
+            close(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    public void receiveMessages() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (socket.isConnected()) {
+                    try {
+                        String message = bufferedReader.readLine();
+                        System.out.println(message);
+                    } catch (IOException e) {
+                        close(socket, bufferedReader, bufferedWriter);
+                    }
+                }
+            }
+        }).start();
+    }
+
+    protected void close(Socket socket, BufferedReader bufferedReader, BufferedWriter bufferedWriter) {
+        try {
+            if(bufferedReader != null)
+                bufferedReader.close();
+            if(bufferedWriter != null)
+                bufferedWriter.close();
+            if(socket != null)
+                socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("What IP do you wish to connect to?" +
                 " (\"localhost\" for own IP)");
-        ip = scanner.nextLine();
+        String ip = scanner.nextLine();
         System.out.println("What port do you wish to connect to?");
-        port = scanner.nextInt();
+        int port = scanner.nextInt();
         System.out.println("What do you wish to call yourself?");
-        name = scanner.next();
-        System.out.println();
+        String username = scanner.next();
 
-        socket = new Socket(ip, port);
+        Socket socket = new Socket(ip, port);
+        Client client = new Client(socket, username);
+        client.receiveMessages();
+        client.sendMessages();
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -1,18 +1,45 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Server extends SendReceive {
+public class Server {
+
     protected ServerSocket serverSocket;
-    public Server() throws IOException {
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer() {
+        try {
+            while (!serverSocket.isClosed()) {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("New client from " + clientSocket.getInetAddress());
+
+                ClientHandler client = new ClientHandler(clientSocket);
+                Thread clientThread = new Thread(client);
+                clientThread.start();
+            }
+        } catch (IOException e) {
+            closeServer();
+        }
+    }
+    protected void closeServer() {
+        try {
+            if(serverSocket != null)
+                serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("What port do you wish to host out of?");
-        port = scanner.nextInt();
-        System.out.println("What do you wish to call yourself?");
-        name = scanner.next();
-        System.out.println();
-
-        serverSocket = new ServerSocket(port);
-        socket = serverSocket.accept();
+        ServerSocket serverSocket = new ServerSocket(scanner.nextInt());
+        Server server = new Server(serverSocket);
+        server.startServer();
     }
 }
